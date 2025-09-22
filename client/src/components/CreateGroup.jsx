a9748f5663c9d1e5bb7fcffd209b065e949e6647
Anant Dubey <work.anantdubey@gmail.com>
2025-09-22 13:20:37 +0530

    create group created

--- client/src/components/CreateGroup.jsx
+++ client/src/components/CreateGroup.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 
 function CreateGroup() {
+    const [admin,setAdmin]=useState("")
+    const [username,setUsername]=useState("")
+
+    const handleSubmit=()=>{}
+
   return (
     <div>
-      
+      <div>
+        <input type="text" value={admin} onChange={(e)=>{setAdmin(e.target.value)}} />
+        <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} />
+        <div>
+            <button onSubmit={}>submit</button>
+        </div>
+      </div>
     </div>
   )
 }
